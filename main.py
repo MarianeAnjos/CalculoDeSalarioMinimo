f2c3801a8a0874d89a8c7d4f7196369b964e5112
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2022-05-25 00:57:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+
+vsm= float(input("informe o valor do salários minimo "))
+vf= float(input("informe o valor do salário do funcionário: "))
+qsm= vf/vsm
+
+print(f'o funcionário recebe {qsm:.1f} salários mininos')
